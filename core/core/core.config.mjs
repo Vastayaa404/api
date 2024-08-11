e2757171d5c1f9e933c4c95ed1e0b90e171c7295
Vastayaa <29062004@inbox.ru>
2024-08-11 22:21:41 +0300

    TO DO

---
+++ core/core/core.config.mjs
@@ -0,0 +1,17 @@
+// Import all dependencies ========================================================================================================>
+import { v4 as uuidv4 } from 'uuid';
+
+// Logic ==========================================================================================================================>
+const corsConfig = {
+    origin: ['http://localhost:3000', 'https://weather-now.ru', 'https://www.weather-now.ru'],
+    methods: ['GET', 'POST', 'OPTIONS'],
+    credentials: true
+};
+
+const headersConfig = (req, res, next) => {
+    req.headers['X-Dora-Request-Id'] = uuidv4();
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
+    next();
+};
+
+export { corsConfig, headersConfig };
--- core/core/core.mjs
+++ core/core/core.mjs
@@ -2,12 +2,11 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import proxy from '@fastify/http-proxy';
-import { v4 as uuidv4 } from 'uuid';
+import { corsConfig, headersConfig } from './core.config.mjs';
 
 // Logic ==========================================================================================================================>
 const fastify = Fastify();
-fastify.register(cors)
-.addHook('onRequest', (req, res, next) => { req.headers['X-Dora-Request-Id'] = uuidv4(); next() }) // ID Header to all requests
+fastify.addHook('onRequest', headersConfig).register(cors, corsConfig)
 .register(proxy, { upstream: 'http://localhost:5001', prefix: '/auth' }) // To auth gateway
 .register(proxy, { upstream: 'http://localhost:5002', prefix: '/services' }) // To project gateway
 .register(proxy, { upstream: 'http://localhost:5010', prefix: '/dev' }) // To dev (test) gateway
